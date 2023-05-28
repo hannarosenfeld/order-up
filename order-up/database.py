5806367e6e75b41bed76087e3dab42fd6fc3c786
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2023-05-28 15:04:14 -0400

    Init.

---
+++ order-up/database.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+# Regardless of the lint error you receive,
+# load_dotenv must run before running this
+# so that the environment variables are
+# properly loaded.
+from app import app, db
+from app.models import Employee, Menu, MenuItem, MenuItemType
+
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+    employee = Employee(name="Margot", employee_number=1234, password="password")
+
+    beverages = MenuItemType(name="Beverages")
+
+    db.session.add(employee)
+    db.session.add(beverages)
+    db.session.commit()
